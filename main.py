0bc3064bfb82430a90a78381f9e58d8ae41db813
Boris Gezkovski <bgezkovski@gmail.com>
2017-09-30 08:24:17 +0300

    Added command line argument --lastpage

--- main.py
+++ main.py
@@ -3,15 +3,25 @@
 # Imports
 from urllib.request import urlopen as uOpen
 import time
+import argparse
 import core
 import utils
 
-print('\nSteamScrape3\n')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--lastpage', dest='lastpage', help='Specify the last page to scrape', type=int)
+args = parser.parse_args()
+
+
+print('\nSteamScrape3 by Boris Gezkovski\n')
 
 # Get the program settings from utils module
 utils.get_settings()
 
+
 print('Settings loaded\n')
+
+
 # Create variable to store page number and set it to 1
 page = 1
 # Create variable to store last page and set it to 2 
@@ -29,7 +39,7 @@ print('File {} created\n'.format(str(filename)))
 start_time = time.time()
 
 static_url = utils.get_url()
-print(static_url)
+
 # loop until page is less than last page
 while page < last_page:
 
@@ -45,10 +55,13 @@ while page < last_page:
     # --Close Url-- 
     uClient.close()
     # Run only if first page
-    if page == 1:
+    if page == 1 and args.lastpage == None:
         # set last page 
         last_page = core.get_last_page(html_source) + 1
-        print('Number of pages:' + str(last_page))
+        print('Number of pages to scrape:' + str(last_page))
+    elif page == 1 and args.lastpage != None:
+        last_page = args.lastpage + 1
+        print('Number of pages to scrape:' + str(args.lastpage))
 
     # Call scrape function from core module giving it the html_source and the file f
     core.scrape(html_source, f) 
